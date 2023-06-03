76023057d5d49fe0d994cf90b8b10ef5ebdecf8d
Sajeed Syed <123931227+mrsajids@users.noreply.github.com>
2023-06-03 20:28:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const cors = require("cors");
-// const path = require("path");
+ const path = require("path");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -14,11 +14,12 @@ app.use(cors())
 //const db = 'mongodb+srv://sajeed:sajeed123@cluster0.ivnebch.mongodb.net/?retryWrites=true&w=majority'
 
 //static files
-// app.use(express.static(path.join(__dirname,'./frontend/build')))
-// app.get('*/',function(req,res){
-//     res.sendFile(path.join(__dirname,'./frontend/build/index.html'))
-// })
+/app.use(express.static(path.join(__dirname,'./frontend/build')))
+app.get('*/',function(req,res){
+  res.sendFile(path.join(__dirname,'./frontend/build/index.html'))
+})
 
+//connection making
 mongoose.connect(process.env.URI).then(() => {
     console.log('connection successful');
     app.listen(process.env.PORT || 5000, (err) => {
